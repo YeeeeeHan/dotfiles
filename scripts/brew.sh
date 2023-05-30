a4f059c0868beac874ad30f0be0bac6ca0f9621f
Lim Yee Han <limyeehan@Lims-MacBook-Air.local>
2023-05-31 00:12:54 +0800

    brew install applications(vscode, notion, spotify, displaylink, tg-desktop, spectacle, chrome

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -2,6 +2,19 @@
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew update
 brew upgrade
 brew install wget
+
+# Install common applications
+brew install --cask visual-studio-code
+brew install --cask notion
+brew install --cask spotify
+brew install --cask displaylink
+brew install --cask telegram-desktop
+brew install --cask spectacle
+brew install --cask google-chrome
+
+# For running dotfiles
+brew install lotyp/formulae/dockutil
